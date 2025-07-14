8a5a5e797848cd2d4518e9a47acae8dea30aec84
datpham-nuoa <dat.pham@nuoa.io>
2025-07-14 14:17:52 +0700

    remove all

--- src/app/data-integrations/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DataIntegrationView() {
-  return (
-    <div>DataIntegrationView</div>
-  )
-}
-
-export default DataIntegrationView
--- src/app/data-management/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DataManagementView() {
-  return (
-    <div>DataManagementView</div>
-  )
-}
-
-export default DataManagementView
